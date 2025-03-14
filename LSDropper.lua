c769418d8acd34e6c45ad2852ce0ce9693c9f6e7
LS-AltControl <a7exlh@pm.me>
2025-03-14 23:33:55 +0100

    Update LSDropper.lua

--- LSDropper.lua
+++ LSDropper.lua
@@ -3,15 +3,19 @@ _G.LSDropper = {
     Controllers = {
         "707293189",
     },
+    
+    -- alts: Put all alt user IDs here
     alts = {
         [1] = 2939174150,
         [2] = 3281293170,
         [3] = 3034352629,
         [4] = 2827160867,
+        -- Add more here if needed, up to [38] etc.
     },
+    
     -- Prefix: Enter the prefix before each command.
     Prefix = "/"
 }
 
--- Loader: Loads the script.
+-- Loader: Loads the main script logic
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LS-AltControl/LS-Control/refs/heads/main/Loader.lua"))()
