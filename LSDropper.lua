d958a67d77e733eccb83a15ddf1bf36f7d4dd4ef
LS-AltControl <a7exlh@pm.me>
2025-03-14 22:43:24 +0100

    Update LSDropper.lua

--- LSDropper.lua
+++ LSDropper.lua
@@ -3,7 +3,12 @@ _G.LSDropper = {
     Controllers = {
         "707293189",
     },
-    
+    alts = {
+        [1] = 2939174150,
+        [2] = 3281293170,
+        [3] = 3034352629,
+        [4] = 2827160867,
+    },
     -- Prefix: Enter the prefix before each command.
     Prefix = "/"
 }
